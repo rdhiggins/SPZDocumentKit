71f1a0cc2d205662dcd142855de4aeb072613aa6
Rodger Higgins <rdhiggins@gmail.com>
2015-05-26 16:09:44 -0500

    Initial Commit

---
+++ SPZDocumentKit/SPZDocumentKit.h
@@ -0,0 +1,19 @@
+//
+//  SPZDocumentKit.h
+//  SPZDocumentKit
+//
+//  Created by Rodger Higgins on 5/26/15.
+//  Copyright (c) 2015 Spazstik Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPZDocumentKit.
+FOUNDATION_EXPORT double SPZDocumentKitVersionNumber;
+
+//! Project version string for SPZDocumentKit.
+FOUNDATION_EXPORT const unsigned char SPZDocumentKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPZDocumentKit/PublicHeader.h>
+
+
